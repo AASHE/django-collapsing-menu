16e7c0ea710c11d0e497c5ab3aee0e7bfaa1175e
jamstooks <jamstooks@localhost>
2013-05-31 14:52:13 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+# Utility function to read README file
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='django-collapsing-menu',
+      version='0.1',
+      description='A template tag for generating a collapsing menu with django using jquery and bootstrap.',
+      author='Benjamin Stookey',
+      author_email='ben@aashe.org',
+      url='https://bitbucket.org/aashe/django-collapsing-menu',
+      license='LICENSE',
+      long_description=read("README.rst"),
+      packages=[
+        'collapsing-menu',
+        'collapsing-menu.templatetags',
+        ],
+      install_requires=[
+              "Django >= 1.3",
+              'setuptools'
+              ],
+      classifiers=[
+              'Development Status :: 4 - Beta',
+              'Environment :: Web Environment',
+              'Framework :: Django',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Topic :: Utilities'
+          ],
+      )
